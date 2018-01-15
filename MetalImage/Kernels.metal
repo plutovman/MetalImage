0c9d9663771af8fde884d9b182dccb0ac7c1827d
plutovman <vladimir@vladimirs-imac.local>
2018-01-14 17:40:58 -0800

    Initial commit by vss

--- MetalImage/Kernels.metal
+++ MetalImage/Kernels.metal
@@ -9,12 +9,27 @@
 #include <metal_stdlib>
 using namespace metal;
 
-kernel void pixelate(texture2d<float, access::read> inTexture [[texture(0)]],
-					 texture2d<float, access::write> outTexture [[texture(1)]],
-					 device unsigned int *pixelSize [[buffer(0)]],
-					 uint2 gid [[thread_position_in_grid]])
+kernel void pixelate(
+                      texture2d<float, access::read> inTexture [[texture(0)]],
+                      texture2d<float, access::write> outTexture [[texture(1)]],
+                      device unsigned int *pixelSize [[buffer(0)]],
+                      uint2 gid [[thread_position_in_grid]])
 {
 	const uint2 pixellateGrid = uint2((gid.x / pixelSize[0]) * pixelSize[0], (gid.y / pixelSize[0]) * pixelSize[0]);
 	const float4 colorAtPixel = inTexture.read(pixellateGrid);
 	outTexture.write(colorAtPixel, gid);
 }
+
+
+kernel void mangleColors(
+                           texture2d<float, access::read> inTexture [[texture(0)]],
+                           texture2d<float, access::write> outTexture [[texture(1)]],
+                           uint2 gid [[thread_position_in_grid]])
+{
+  const float4 colorAtPixel = inTexture.read(gid);
+  const float4 outputColor = float4(colorAtPixel.b, colorAtPixel.r,
+                                    colorAtPixel.g, 1);
+  outTexture.write(outputColor, gid);
+}
+
+
--- MetalImage/ViewController.swift
+++ MetalImage/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Metal
 import MetalKit
+import MetalPerformanceShaders
 
 class ViewController: UIViewController {
 
@@ -16,18 +17,19 @@ class ViewController: UIViewController {
 	@IBOutlet var pixelSizeSlider: UISlider!
 
 	var pixelSize: UInt = 60
+  
 
-	@IBAction func changePixelSize(sender: AnyObject) {
+	@IBAction func changePixelSize(_ sender: AnyObject) {
 		if let slider = sender as? UISlider {
 			pixelSize = UInt(slider.value)
 
-			dispatch_async(queue) {
+			queue.async {
 
-				self.applyFilter()
+				self.applyFilterMPSCompound()
 
-				let finalResult = self.imageFromTexture(self.outTexture)
+				let finalResult = self.image(from: self.outTexture)
 
-				dispatch_async(dispatch_get_main_queue()) {
+				DispatchQueue.main.async {
 					self.imageView.image = finalResult
 				}
 
@@ -37,12 +39,10 @@ class ViewController: UIViewController {
 	}
 
 	/// The queue to process Metal
-	let queue = dispatch_queue_create("com.invasivecode.metalQueue", DISPATCH_QUEUE_SERIAL)
+	let queue = DispatchQueue(label: "com.invasivecode.metalQueue")
 
 	/// A Metal device
-	lazy var device: MTLDevice! = {
-		MTLCreateSystemDefaultDevice()
-	}()
+	lazy var device: MTLDevice! = MTLCreateSystemDefaultDevice()
 
 	/// A Metal library
 	lazy var defaultLibrary: MTLLibrary! = {
@@ -52,25 +52,27 @@ class ViewController: UIViewController {
 	/// A Metal command queue
 	lazy var commandQueue: MTLCommandQueue! = {
 		NSLog("\(self.device.name!)")
-		return self.device.newCommandQueue()
+		return self.device.makeCommandQueue()
 	}()
 
 	var inTexture: MTLTexture!
 	var outTexture: MTLTexture!
+  var intermediateTexture: MTLTexture!
 	let bytesPerPixel: Int = 4
 
 	/// A Metal compute pipeline state
 	var pipelineState: MTLComputePipelineState!
 
 	func setUpMetal() {
-		if let kernelFunction = defaultLibrary.newFunctionWithName("pixelate") {
+    if let kernelFunction = defaultLibrary.makeFunction(name: "pixelate") {
 			do {
-				pipelineState = try device.newComputePipelineStateWithFunction(kernelFunction)
+				pipelineState = try device.makeComputePipelineState(function: kernelFunction)
 			}
 			catch {
 				fatalError("Impossible to setup Metal")
 			}
 		}
+    
 	}
 
 	let threadGroupCount = MTLSizeMake(16, 16, 1)
@@ -82,46 +84,54 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		dispatch_async(queue) {
+		queue.async {
 			self.setUpMetal()
 		}
 	}
 
-	override func viewDidAppear(animated: Bool) {
+
+	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 
-		dispatch_async(queue) { () -> Void in
+		queue.async { () -> Void in
 
 			self.importTexture()
 
-			self.applyFilter()
+			self.applyFilterMPSCompound()
 
-			let finalResult = self.imageFromTexture(self.outTexture)
-			dispatch_async(dispatch_get_main_queue(), { () -> Void in
+			let finalResult = self.image(from: self.outTexture)
+			DispatchQueue.main.async {
 				self.imageView.image = finalResult
-			})
+			}
 
 		}
 	}
+
 
 	func importTexture() {
 		guard let image = UIImage(named: "invasivecode") else {
 			fatalError("Can't read image")
 		}
-		inTexture = textureFromImage(image)
+		inTexture = texture(from: image)
+    
+    let imageSize: CGSize = (image.size)
+    let intermediateTextureDesciptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: MTLPixelFormat.rgba8Unorm, width: Int(imageSize.width), height: Int(imageSize.height), mipmapped: false)
+    
+    intermediateTexture = device.makeTexture(descriptor: intermediateTextureDesciptor)
 	}
+
 
 	func applyFilter() {
 
-		let commandBuffer = commandQueue.commandBuffer()
-		let commandEncoder = commandBuffer.computeCommandEncoder()
+		let commandBuffer = commandQueue.makeCommandBuffer()
+		let commandEncoder = commandBuffer.makeComputeCommandEncoder()
 
 		commandEncoder.setComputePipelineState(pipelineState)
-		commandEncoder.setTexture(inTexture, atIndex: 0)
-		commandEncoder.setTexture(outTexture, atIndex: 1)
+		commandEncoder.setTexture(inTexture, at: 0)
+		commandEncoder.setTexture(outTexture, at: 1)
 
-		let buffer = device.newBufferWithBytes(&pixelSize, length: sizeof(UInt), options: [MTLResourceOptions.StorageModeShared])
-		commandEncoder.setBuffer(buffer, offset: 0, atIndex: 0)
+		let buffer = device.makeBuffer(bytes: &pixelSize, length: MemoryLayout<UInt>.size, options: MTLResourceOptions.storageModeShared)
+		commandEncoder.setBuffer(buffer, offset: 0, at: 0)
 
 		commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
 		commandEncoder.endEncoding()
@@ -129,18 +139,35 @@ class ViewController: UIViewController {
 		commandBuffer.commit()
 		commandBuffer.waitUntilCompleted()
 	}
+  
+  func applyFilterMPSCompound() {
+    
+    let commandBuffer = commandQueue.makeCommandBuffer()
+    let blur = MPSImageGaussianBlur(device: device, sigma: Float(pixelSize))
+    
+    let sobel = MPSImageSobel(device: device)
+    
+    sobel.encode(commandBuffer: commandBuffer, sourceTexture: inTexture, destinationTexture: intermediateTexture)
+    blur.encode(commandBuffer: commandBuffer, sourceTexture: intermediateTexture, destinationTexture: outTexture)
+    
+    commandBuffer.commit()
+    commandBuffer.waitUntilCompleted()
 
-	func textureFromImage(image: UIImage) -> MTLTexture {
+    
+  }
 
-		guard let cgImage = image.CGImage else {
+
+	func texture(from image: UIImage) -> MTLTexture {
+
+		guard let cgImage = image.cgImage else {
 			fatalError("Can't open image \(image)")
 		}
 
 		let textureLoader = MTKTextureLoader(device: self.device)
 		do {
-			let textureOut = try textureLoader.newTextureWithCGImage(cgImage, options: nil)
-			let textureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(textureOut.pixelFormat, width: textureOut.width, height: textureOut.height, mipmapped: false)
-			outTexture = self.device.newTextureWithDescriptor(textureDescriptor)
+			let textureOut = try textureLoader.newTexture(with: cgImage)
+			let textureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: textureOut.pixelFormat, width: textureOut.width, height: textureOut.height, mipmapped: false)
+			outTexture = self.device.makeTexture(descriptor: textureDescriptor)
 			return textureOut
 		}
 		catch {
@@ -149,24 +176,30 @@ class ViewController: UIViewController {
 	}
 
 
-	func imageFromTexture(texture: MTLTexture) -> UIImage {
+	func image(from texture: MTLTexture) -> UIImage {
 
 		let imageByteCount = texture.width * texture.height * bytesPerPixel
 		let bytesPerRow = texture.width * bytesPerPixel
-		var src = [UInt8](count: Int(imageByteCount), repeatedValue: 0)
+		var src = [UInt8](repeating: 0, count: Int(imageByteCount))
 
 		let region = MTLRegionMake2D(0, 0, texture.width, texture.height)
-		texture.getBytes(&src, bytesPerRow: bytesPerRow, fromRegion: region, mipmapLevel: 0)
+		texture.getBytes(&src, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
 
-		let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.ByteOrder32Big.rawValue | CGImageAlphaInfo.PremultipliedLast.rawValue))
+		let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue))
 
-		let grayColorSpace = CGColorSpaceCreateDeviceRGB()
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
 		let bitsPerComponent = 8
-		let context = CGBitmapContextCreate(&src, texture.width, texture.height, bitsPerComponent, bytesPerRow, grayColorSpace, bitmapInfo.rawValue);
+		let context = CGContext(data: &src,
+		                        width: texture.width,
+		                        height: texture.height,
+		                        bitsPerComponent: bitsPerComponent,
+		                        bytesPerRow: bytesPerRow,
+		                        space: colorSpace,
+		                        bitmapInfo: bitmapInfo.rawValue)
 
-		let dstImageFilter = CGBitmapContextCreateImage(context);
+		let dstImageFilter = context?.makeImage()
 		
-		return UIImage(CGImage: dstImageFilter!, scale: 0.0, orientation: UIImageOrientation.DownMirrored)
+		return UIImage(cgImage: dstImageFilter!, scale: 0.0, orientation: UIImageOrientation.up)
 	}
 	
 }
